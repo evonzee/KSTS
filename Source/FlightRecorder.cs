60e187e1353fba6ef3fa849cf0d94bef70ed3749
eric vz <gitkraken@base10.org>
2018-05-27 08:42:24 -0500

    Tweak for atmosphere-less bodies

--- Source/FlightRecorder.cs
+++ Source/FlightRecorder.cs
@@ -292,6 +292,9 @@ namespace KSTS
             // Save the minimum altitude we need for a stable orbit as well as the launch-body's name:
             launchBodyName = vessel.mainBody.bodyName;
             FloatCurve pressureCurve = vessel.mainBody.atmospherePressureCurve;
+            if(pressureCurve.Curve.length == 0) {
+                minAltitude = 1; // if there's no atmosphere, theoretically we could orbit at 1m
+            }
             for (int i = pressureCurve.Curve.length - 1; i >= 0; i--)
             {
                 if (pressureCurve.Curve[i].value == 0)
